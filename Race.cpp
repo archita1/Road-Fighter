f0501006850d8ad7554f5fc26e061fbb96a1f7aa
archita-jindal <archita.jindal@gmail.com>
2014-05-15 12:41:54 +0530

    Design changed

--- Race.cpp
+++ Race.cpp
@@ -6,12 +6,12 @@
 #include<stdlib.h>
 
 void makecar(int x, int y)
- {
-  rectangle(x+1,y,x+49,y+100);
-  rectangle(x+1,y+25,x+49,y+75);
-  setfillstyle(SOLID_FILL,8);
-  floodfill((x+x+50)/2,(y+y+100)/2,15);
-  }
+{
+    rectangle(x+1,y,x+49,y+100);
+    rectangle(x+1,y+25,x+49,y+75);
+    setfillstyle(SOLID_FILL,8);
+    floodfill((x+x+50)/2,(y+y+100)/2,15);
+}
 
 void clear(int x,int y)
  {
@@ -27,35 +27,55 @@ void main()
  {
   int driver,mode;
   driver = DETECT;
-  initgraph(&driver,&mode,"C:\\TurboC3\\BGI");
+  initgraph(&driver,&mode,"c:\\TurboC3\\bgi");
   int x=150,y=350,ch,life=3,score=0;
   int level=1;
   char choice;
+/*  cout<<"\n\n\n\n\n\t\t\t* THUNDER RACER *";
+  cout<<"\n\t\t\t ---------------";
+  cout<<"\n\n\tLong long ago the state of Valencia was attacked by the";
+  cout<<"\n\n\tenemies and were defeated. Every soldier and citizen was";
+  cout<<"\n\n\tkilled except the beautiful princess Cindrella  and you";
+  cout<<"\n\n\twho survived. Enemies want to kill her and she is in your";
+  cout<<"\n\n\tcar right now. If you have the guts save her from enemies'";
+  cout<<"\n\n\tcars and marry her.";
+  cout<<"\n\n\tSo all the best...";
+  */
+  //delay(2000);
   for(int m=1;m<25;m++)
 	for(int n=1;n<80;n++) {
 		gotoxy(n,m);
 		cout<<" ";
 		}
   setcolor(RED);
-  rectangle(0,60,200,120);
+  rectangle(0,60,120,120);
+  rectangle(0,300,120,420);
   gotoxy(5,21);
+  //cout<<"Only Ameya & Prateek";
+  gotoxy(5,22);
+  //cout<<"have saved her till";
+  gotoxy(5,23);
+  //cout<<"now. So you can be";
+  gotoxy(5,24);
+  //cout<<"the 3-rd";
   rectangle(150,0,401,getmaxy());
   setfillstyle(SOLID_FILL,8);
   floodfill(325,getmaxy()/2,RED);
   setcolor(RED);
-  rectangle(20,200,200,250);
+  rectangle(0,200,120,250);
   gotoxy(2,15);
   cout<<"<Esc> -> Exit";
+  //for(int level=1;(level<=5)&&(life>0);level++)
   {
 	if(level==1){
-	  gotoxy(5,5);
+	  gotoxy(2,5);
 	  cout<<"Starts Now";
-	  gotoxy(5,7);
+	  gotoxy(2,7);
 	  cout<<"All the best";
-	  delay(5000);
-	  gotoxy(5,5);
-	  cout<<"                    ";
-	  gotoxy(5,7);
+	  delay(500);
+	  gotoxy(2,5);
+	  cout<<"          ";
+	  gotoxy(2,7);
 	  cout<<"            ";
 	  }
 	 else {
@@ -78,7 +98,9 @@ void main()
 		exit(0);
 	   }
 	setcolor(RED);
-
+	rectangle(420,125,600,175);
+	gotoxy(55,10);
+	cout<<"Level = "<<level;
 	rectangle(420,250,600,300);
 	gotoxy(55,18);
 	cout<<"Lives = "<<life;
@@ -86,17 +108,25 @@ void main()
 	gotoxy(55,24);
 	cout<<"Score = "<<score;
 	int accident=0;
-	int y1=1,x1=250+((rand()%3)*50);
-	int y2=1,x2=250+((rand()%3)*50);
+	int y1=1,x1=150+((rand()%5)*50);
+	int y2=1,x2=150+((rand()%5)*50);
+	int y3=1,x3=150+((rand()%5)*50);
+	int y4=1,x4=150+((rand()%5)*50);
 	score += 10;
 	while(y1<getmaxy()-1)
 	{
 		clear(x1,y1);
 		clear(x2,y2);
+		clear(x3,y3);
+		clear(x4,y4);
 		y1++;
 		y2++;
+		y3++;
+		y4++;
 		makecar(x1,y1);
 		makecar(x2,y2);
+		makecar(x3,y3);
+		makecar(x4,y4);
 		makecar(x,y);
 		delay(5-level);
 		if(kbhit())
@@ -107,12 +137,12 @@ void main()
 			case 27: exit(0);
 			break;
 			case 75:clear(x,y);
-				if(x>250)
+				if(x>150)
 					x=x-50;
 				makecar(x,y);
 			break;
 			case 77:clear(x,y);
-				if(x<350)
+				if(x<450)
 					x=x+50;
 				makecar(x,y);
 			break;
@@ -129,13 +159,17 @@ void main()
 	    if(life==0) {
 	     gotoxy(5,5);
 	     cout<<"GAME OVER ";
+	     gotoxy(5,6);
+	     cout<<"You could not save";
+	     gotoxy(5,7);
+	     cout<<"her";
 	     break;
 	     }
-	    gotoxy(5,5);
-	    cout<<"You have lost 1 life";
+	    gotoxy(2,5);
+	    cout<<"Lost 1 life";
 	    delay(3000);
-	    gotoxy(5,5);
-	    cout<<"                    ";
+	    gotoxy(2,5);
+	    cout<<"           ";
 	    }
   }
   }
